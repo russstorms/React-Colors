3dd8083e12180b89103922c987a1db17329669c7
Russ Storms <rstorms90@gmail.com>
2019-12-29 15:52:41 -0700

    work in to read from seedColors js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,31 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
 import seedColors from './seedColors';
-import { generatePalette } from "./colorHelpers";
+import { generatePalette } from './colorHelpers';
 
 class App extends Component {
+
+  findPalette(id) {
+    return seedColors.find(function(palette) {
+      return palette.id === id;
+    })
+  }
+
   render() {
     return (
       <Switch>
-        <Route exact path="/" render={() => <h1>PALETTE HERE</h1>} />
-        <Route exact path="/palette/:id" render={() => <h1>Individual Palette</h1>} />
+        <Route exact path='/' render={() => <h1>PALETTE HERE</h1>} />
+        <Route
+          exact
+          path='/palette/:id'
+          render={routeProps => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )} 
+            />
+          )} 
+        />
       </Switch>
 
       // <div>
