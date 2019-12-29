034424acc30456c2f63e89324764b48dcfe00351
Russ Storms <rstorms90@gmail.com>
2019-12-29 13:35:42 -0700

    copy functionality and color helper to generate colors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Palette from './Palette';
 import seedColors from './seedColors';
-import './App.css';
+import { generatePalette } from "./colorHelpers";
 
-function App() {
-  return (
-    <div className="App">
-      <Palette {...seedColors[4]} />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Palette {...seedColors[4]} />
+      </div>
+    );
+  }
 }
 
 export default App;
