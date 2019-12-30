cc1d84c0442a76057e177cab67babe37c4b4bee6
Russ Storms <rstorms90@gmail.com>
2019-12-30 09:24:14 -0700

    palette links from landing page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
+import PaletteList from './PaletteList';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 
@@ -15,7 +16,7 @@ class App extends Component {
   render() {
     return (
       <Switch>
-        <Route exact path='/' render={() => <h1>PALETTE HERE</h1>} />
+        <Route exact path='/' render={() => <PaletteList palettes={seedColors} />} />
         <Route
           exact
           path='/palette/:id'
@@ -28,10 +29,6 @@ class App extends Component {
           )} 
         />
       </Switch>
-
-      // <div>
-      //   <Palette palette={generatePalette(seedColors[4])}/>
-      // </div>
     );
   }
 }
